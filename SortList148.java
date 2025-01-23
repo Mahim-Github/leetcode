c2ac6d5740983694e78bfcf0d59f93416e0c7437
Mahim-Github <139007842+Mahim-Github@users.noreply.github.com>
2025-01-23 19:31:15 +0530

    Sort List 148

---
+++ SortList148.java
@@ -0,0 +1,63 @@
+// 148. Sort List
+// Solved
+// Medium
+// Topics
+// Companies
+// Given the head of a linked list, return the list after sorting it in ascending order.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [4,2,1,3]
+// Output: [1,2,3,4]
+// Example 2:
+
+
+// Input: head = [-1,5,3,4,0]
+// Output: [-1,0,3,4,5]
+// Example 3:
+
+// Input: head = []
+// Output: []
+ 
+
+// Constraints:
+
+// The number of nodes in the list is in the range [0, 5 * 104].
+// -105 <= Node.val <= 105
+
+class Solution {
+    public ListNode sortList(ListNode head) {   
+        if(head==null || head.next==null) return head;   
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode mid = slow.next;
+        slow.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+    
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
+    }
+}
