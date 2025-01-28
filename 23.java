9e4238caab66bc622f650cd91a120df3657a166c
Mahim-Github <139007842+Mahim-Github@users.noreply.github.com>
2025-01-28 22:20:53 +0530

    23 Merge k Sorted Lists

---
+++ 23.java
@@ -0,0 +1,61 @@
+// 23. Merge k Sorted Lists
+// Solved
+// Hard
+// Topics
+// Companies
+// You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+// Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+// Example 1:
+
+// Input: lists = [[1,4,5],[1,3,4],[2,6]]
+// Output: [1,1,2,3,4,4,5,6]
+// Explanation: The linked-lists are:
+// [
+//   1->4->5,
+//   1->3->4,
+//   2->6
+// ]
+// merging them into one sorted list:
+// 1->1->2->3->4->4->5->6
+// Example 2:
+
+// Input: lists = []
+// Output: []
+// Example 3:
+
+// Input: lists = [[]]
+// Output: []
+ 
+
+// Constraints:
+
+// k == lists.length
+// 0 <= k <= 104
+// 0 <= lists[i].length <= 500
+// -104 <= lists[i][j] <= 104
+// lists[i] is sorted in ascending order.
+// The sum of lists[i].length will not exceed 104.
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        List<Integer> values = new ArrayList<>();
+        for (ListNode list : lists) {
+            while (list != null) {
+                values.add(list.val);
+                list = list.next;
+            }
+        }
+        Collections.sort(values);
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+        for (int val : values) {
+            current.next = new ListNode(val);
+            current = current.next;
+        }
+        return dummy.next;
+    }
+}
